1d5621c4c83559d0c44ebd5e89068a1ad62f5990
Elizabeth Vista <elizabeth.vista@perficient.com>
2017-09-18 15:05:52 -0500

    error in App.js

--- App.js
+++ App.js
@@ -118,7 +118,7 @@ export default class App extends Component {
                 this.setState({peripherals});
               }
               console.log('Connected to ' + peripheral.id);
-            }
+            })
           }
         }
     }
